a613a48cf5ab6753f85c91ab198a4e3182bf8e8d
Nick Hammell <nicholas.a.hammell@hotmail.com>
2022-04-16 13:39:19 -0400

    Change grid gen and add slider

--- app.js
+++ app.js
@@ -1,22 +1,33 @@
 const container = document.getElementById('container');
 const clearBtn = document.getElementById('clear');
+const slider = document.getElementById('slider');
 
-function generateSquares() {
-    for (i = 256; i > 0; i--) {
-        let div = document.createElement('div');
-        div.classList.add('box');
-        container.appendChild(div);
-        div.addEventListener('mouseover', function (e) {
-            div.style.backgroundColor = 'black';
+let canvasSize = 16;
+
+slider.onchange = function () {
+    canvasSize = this.value;
+    container.innerHTML = '';
+    generateSquares(canvasSize);
+}
+
+function generateSquares(size) {
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    for (i = 0; i < (size * size); i++) {
+        let pixel = document.createElement('div');
+        pixel.style.transitionDuration = '0.25s';
+        pixel.addEventListener('mouseover', function () {
+            pixel.style.backgroundColor = 'black';
         });
+        container.appendChild(pixel);
     }
 }
 
 function removeColor() {
-    let pixels = document.querySelectorAll('.box');
-    pixels.forEach(pixel => pixel.style.backgroundColor = 'white');
+    container.childNodes.forEach(pixel => pixel.style.backgroundColor = 'white');
 }
 
 clearBtn.addEventListener('click', removeColor);
 
-generateSquares();
+generateSquares(canvasSize);
